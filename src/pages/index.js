37d0e26a0f8bb6d56b2257d771f8c683c61baa83
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2021-05-10 11:27:12 +0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,7 @@ export default function Home() {
     <div className="header">
       <h1 className="headertxt"><hr style={{ color: 'white', maxWidth: '430px' }}></hr>Poltergeist Soft staking plans <hr style={{ color: 'white', maxWidth: '430px' }}></hr></h1>
     </div>
-     
-     <SpacingGrid/>
+     <NewPage/>
     <div style={{ color: 'white', textAlign: 'end', marginRight: '20px',position:'relative', marginTop:'20px' }}>
       <div style={{ textTransform: 'uppercase', color: 'white', fontSize: '18px' }}>
         <img src="https://poltergeistexchange.com/static/media/logo.c4de8924.svg" alt="Poltergeist Exchange" height="25" style={{ marginRight: '8px',  }} />Poltergeist Exchange</div>
