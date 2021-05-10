30a1ee20fb23d978344c38beb9b6463dee265dba
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2021-05-10 12:26:41 +0500

    final update

--- src/pages/NewPage.jsx
+++ src/pages/NewPage.jsx
@@ -4,35 +4,35 @@ export default function NewPage() {
     return (
         <div>
             <div className="container group">
-                <div style={{display:'flex'}}>
-                <div className="pre-grid"><h1>Ghoul Staking Plans</h1><p>Stake your Ghoul on Poltergeist Exchange to earn profit sharing and other upcoming benefits.<br /><br /><a href className="btn-buy">Buy GHOUL</a><br /></p></div>
-               
-                <div className="grid-1-3" style={{marginLeft:'10px'}}>
-                    <img className="goul-icon-a" src="https://poltergeistexchange.com/static/media/logo.c4de8924.svg" alt="Poltergeist Exchange" height={50} />
-                    <h2>Diamond</h2>
-                    <h3><sup>Under </sup>50,000 GHOUL</h3>
-                    <hr /><br />
-                    <ul>
-                        <li>Poltergeist Profit Sharing</li>
-                        <li>Surprise Airdrops</li>
-                        <li>Governance Voting</li>
-                    </ul>
-                    <a href className="button">Stake Now</a>
+                <div style={{ display: 'flex' }}>
+                    <div className="pre-grid"><h1>Ghoul Staking Plans</h1><p>Stake your Ghoul on Poltergeist Exchange to earn profit sharing and other upcoming benefits.<br /><br /><a href className="btn-buy">Buy GHOUL</a><br /></p></div>
+
+                    <div className="grid-1-3" style={{ marginLeft: '10px' }}>
+                        <img className="goul-icon-a" src="https://poltergeistexchange.com/static/media/logo.c4de8924.svg" alt="Poltergeist Exchange" height={50} />
+                        <h2>Diamond</h2>
+                        <h3><sup>Under </sup>50,000 GHOUL</h3>
+                        <hr /><br />
+                        <ul>
+                            <li>Poltergeist Profit Sharing</li>
+                            <li>Surprise Airdrops</li>
+                            <li>Governance Voting</li>
+                        </ul>
+                        <a href className="button">Stake Now</a>
+                    </div>
+                    <div className="grid-1-3" style={{ marginLeft: '10px' }}>
+                        <img className="goul-icon-b" src="https://poltergeistexchange.com/static/media/logo.c4de8924.svg" alt="Poltergeist Exchange" height={50} />
+                        <h2>Platinum</h2>
+                        <h3><sup>Over </sup>50,000 GHOUL</h3>
+                        <hr /><br />
+                        <ul>
+                            <li>Poltergeist Profit Sharing</li>
+                            <li>Exclusive Airdrops</li>
+                            <li>Governance Voting</li>
+                            <li>Early access to PRIFI Dapps</li>
+                        </ul>
+                        <a href className="button">Stake Now</a>
+                    </div>
                 </div>
-                <div className="grid-1-3" style={{marginLeft:'10px'}}>
-                    <img className="goul-icon-b" src="https://poltergeistexchange.com/static/media/logo.c4de8924.svg" alt="Poltergeist Exchange" height={50} />
-                    <h2>Platinum</h2>
-                    <h3><sup>Over </sup>50,000 GHOUL</h3>
-                    <hr /><br />
-                    <ul>
-                        <li>Poltergeist Profit Sharing</li>
-                        <li>Exclusive Airdrops</li>
-                        <li>Governance Voting</li>
-                        <li>Early access to PRIFI Dapps</li>
-                    </ul>
-                    <a href className="button">Stake Now</a>
-                </div>
-            </div>
             </div>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,25 @@
 import React from "react"
-import Body from "./Body"
 import Footer from "./Footer"
-import SpacingGrid from "./gridView"
 import Header from "./Header"
 import NewPage from './NewPage'
 export default function Home() {
   return <div>
-   <div>
-     <Header/>
-   </div>
+    <div>
+      <Header />
+    </div>
     <div className="header">
       <h1 className="headertxt"><hr style={{ color: 'white', maxWidth: '430px' }}></hr>Poltergeist Soft staking plans <hr style={{ color: 'white', maxWidth: '430px' }}></hr></h1>
     </div>
-     <NewPage/>
-    <div style={{ color: 'white', textAlign: 'end', marginRight: '20px',position:'relative', marginTop:'20px' }}>
+    <NewPage />
+    <div style={{ color: 'white', textAlign: 'end', marginRight: '20px', position: 'relative', marginTop: '20px' }}>
       <div style={{ textTransform: 'uppercase', color: 'white', fontSize: '18px' }}>
-        <img src="https://poltergeistexchange.com/static/media/logo.c4de8924.svg" alt="Poltergeist Exchange" height="25" style={{ marginRight: '8px',  }} />Poltergeist Exchange</div>
+        <img src="https://poltergeistexchange.com/static/media/logo.c4de8924.svg" alt="Poltergeist Exchange" height="25" style={{ marginRight: '8px', }} />Poltergeist Exchange</div>
     </div>
     <div>
-     
-     
     </div>
     <div>
-
-
-      
-      <Footer/>
+      <Footer />
     </div>
-   
+
   </div>
 }
